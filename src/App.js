502f7fc2867869c547dbd7e3ae6f3861086eeccc
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2019-04-07 23:30:43 +0900

    stateを使用した状態に変更

--- src/App.js
+++ src/App.js
@@ -2,26 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userName: "userName",
+      totalGoods: 100,
+      userIcon: "./猫.png",
+      contributions: ["./猫.png","./猫.png","./猫.png","./猫.png","./猫.png","./猫.png"],
+    }
+  }
   render() {
     return (
       <div className="profile">
         <div className="userHeader">
-          <img className="userIcon" src="./猫.png" />
+          <img className="userIcon" src={this.state.userIcon} />
           <div className="userName">
-          username
+          {this.state.userName}
           </div>
           <div className="totalGoods">
           「いいね！」
-          100
+          {this.state.totalGoods}
           </div>
         </div>
         <div className="contributions">
-        <img className="contribution" src="./猫.png" />
-        <img className="contribution" src="./猫.png" />
-        <img className="contribution" src="./猫.png" />
-        <img className="contribution" src="./猫.png" />
-        <img className="contribution" src="./猫.png" />
-        <img className="contribution" src="./猫.png" />
+          {this.state.contributions.map((contribution, index) => {
+            return <img key={index} className="contribution" src={contribution} />
+          })}
         </div>
       </div>
     );
