4084747b93c455e1ba062549e789dbbfcde7d158
YukiYanagikubo <yuki.giga.baseball1@gmail.com>
2019-04-07 23:04:34 +0900

    静的ページの作成

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="profile">
+        <div className="userHeader">
+          <img className="userIcon" src="./猫.png" />
+          <div className="userName">
+          username
+          </div>
+          <div className="totalGoods">
+          「いいね！」
+          100
+          </div>
+        </div>
+        <div className="contributions">
+        <img className="contribution" src="./猫.png" />
+        <img className="contribution" src="./猫.png" />
+        <img className="contribution" src="./猫.png" />
+        <img className="contribution" src="./猫.png" />
+        <img className="contribution" src="./猫.png" />
+        <img className="contribution" src="./猫.png" />
+        </div>
       </div>
     );
   }
